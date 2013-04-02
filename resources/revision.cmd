c212b173922fe95bb18ae2e24f55c5c1bf7c6ee0
Ghabry <gabelk@web.de>
2013-04-02 19:04:20 +0200

    Update revision scripts to handle case when .git is missing (e.g. in source tarballs)

--- resources/revision.cmd
+++ resources/revision.cmd
@@ -1,10 +1,21 @@
 @echo off
-:: Writes current git revision in src/revision.h
+:: If git repository:
+::  Writes current git revision in src/revision.h
+:: If not a git repository (e.g. source tarball):
+::  revision.h exists: Do nothing
+::  revision.h missing: Write version "Unknown Version" in src/revision.h
 
 pushd %~dp0\..
 
-for /F "tokens=*" %%a in ('git describe --abbrev^=4 --long') do (
-    SET GITREV=#define PLAYER_REVISION "%%a"
+IF NOT EXIST .git (
+    IF EXIST src/revision.h (
+        goto end
+    )    
+    SET GITREV=#define PLAYER_REVISION "Unknown Version"
+) ELSE (
+    for /F "tokens=*" %%a in ('git describe --abbrev^=4 --long') do (
+        SET GITREV=#define PLAYER_REVISION "%%a"
+    )
 )
 
 cd /d src
@@ -14,4 +25,5 @@ echo #define EASYRPG_PLAYER_REVISION_H >> revision.h
 echo %GITREV% >> revision.h
 echo #endif >> revision.h
 
+:end
 popd
--- resources/revision.sh
+++ resources/revision.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
+# If git repository:
+#  Writes current git revision in src/revision.h
+# If not a git repository (e.g. source tarball):
+#  revision.h exists: Do nothing
+#  revision.h missing: Write version "Unknown Version" in src/revision.h
 
 SCRIPT_DIR="`dirname \"$0\"`"
-GITREV=`git describe --abbrev=4 --long`
+
+cd $SCRIPT_DIR/..
+
+if [ -d ".git" ]
+then
+    GITREV=`git describe --abbrev=4 --long`
+else
+    if [ -e src/revision.h ]
+    then
+        exit
+    fi
+    GITREV="Unknown Version"
+fi
 
 cd $SCRIPT_DIR/../src
 
