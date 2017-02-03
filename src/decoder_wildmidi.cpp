2324ca9f14ea2d4f39b9e6e81783edc6048250d7
Christian Breitwieser <christian.breitwieser@gmail.com>
2017-02-03 19:26:49 +0100

    Now builds on linux, decoder_xmp now loads mod files from a buffer filled with a istream, decoder_wav does now work with streams, decoder_wildmidi is (apart from the interface) unchanged.

--- src/decoder_wildmidi.cpp
+++ src/decoder_wildmidi.cpp
@@ -296,7 +296,7 @@ bool WildMidiDecoder::WasInited() const {
 	return init;
 }
 
-bool WildMidiDecoder::Open(FILE* file) {
+bool WildMidiDecoder::Open(std::shared_ptr<FileFinder::istream> stream) {
 	if (!init)
 		return false;
 
@@ -312,7 +312,6 @@ bool WildMidiDecoder::Open(FILE* file) {
 		return false;
 	}
 
-	fclose(file);
 	return true;
 }
 
