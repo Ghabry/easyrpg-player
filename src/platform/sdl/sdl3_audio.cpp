d1d67479badb64f6e9e60262f5d628a2711797e6
Ghabry <gabriel+github@mastergk.de>
2023-10-27 21:46:03 +0200

    SDL3: Add new audio backend
    SDL3 has now built-in mixing, making our generic decoder obsolete.

--- src/platform/sdl/sdl3_audio.cpp
+++ src/platform/sdl/sdl3_audio.cpp
@@ -17,76 +17,39 @@
 
 #include "system.h"
 
-#ifdef SUPPORT_AUDIO
-
-#include <cassert>
-#include <cstdint>
-#include <chrono>
-#include <SDL.h>
 #include <SDL_audio.h>
-#include <SDL_version.h>
-
-#ifdef EMSCRIPTEN
-#  include <emscripten.h>
-#endif
-
+#include <SDL_error.h>
+#include <chrono>
+#include <cstring>
+#include <cassert>
+#include <memory>
 #include "sdl3_audio.h"
 #include "output.h"
 
-using namespace std::chrono_literals;
-
-namespace {
-	SDL_AudioStream* audio_stream = nullptr;
-}
-
-void sdl_audio_callback(void* userdata, SDL_AudioStream* stream, int additional_amount, [[maybe_unused]] int total_amount) {
-	// no mutex locking required, SDL does this before calling
-	if (additional_amount <= 0) {
-		return;
-	}
-
-	static std::vector<uint8_t> buffer;
-	if (buffer.size() < static_cast<size_t>(additional_amount)) {
-		buffer.resize(additional_amount);
-	}
-
-	// FIXME MIGRATION: Audio crackles
-	static_cast<GenericAudio*>(userdata)->Decode(buffer.data(), buffer.size());
-
-	SDL_PutAudioStreamData(stream, buffer.data(), buffer.size());
-}
-
-AudioDecoder::Format sdl_format_to_format(Uint16 format) {
+SDL_AudioFormat to_sdl_format(AudioDecoder::Format format) {
 	switch (format) {
-		case SDL_AUDIO_U8:
-			return AudioDecoder::Format::U8;
-		case SDL_AUDIO_S8:
-			return AudioDecoder::Format::S8;
-		case SDL_AUDIO_S16:
-			return AudioDecoder::Format::S16;
-		case SDL_AUDIO_S32:
-			return AudioDecoder::Format::S32;
-		case SDL_AUDIO_F32:
-			return AudioDecoder::Format::F32;
+		case AudioDecoder::Format::S8:
+			return SDL_AUDIO_S8;
+		case AudioDecoder::Format::U8:
+			return SDL_AUDIO_U8;
+		case AudioDecoder::Format::S16:
+			return SDL_AUDIO_S16;
+		case AudioDecoder::Format::S32:
+			return SDL_AUDIO_S32;
+		case AudioDecoder::Format::F32:
+			return SDL_AUDIO_F32;
 		default:
-			assert(false);
+			return 0;
 	}
-
-	return (AudioDecoder::Format)-1;
 }
 
-Sdl3Audio::Sdl3Audio(const Game_ConfigAudio& cfg) :
-	GenericAudio(cfg)
-{
+Sdl3Audio::Sdl3Audio(const Game_ConfigAudio& cfg) : AudioInterface(cfg) {
 	if (SDL_InitSubSystem(SDL_INIT_AUDIO) < 0) {
 		Output::Warning("Couldn't init audio: {}", SDL_GetError());
 		return;
 	}
 
-#ifdef GEKKO
-	// Wii's DSP works at 32kHz natively
-	const int frequency = 32000;
-#elif defined(EMSCRIPTEN)
+#if defined(EMSCRIPTEN)
 	// Get preferred sample rate from Browser (-> OS)
 	const int frequency = EM_ASM_INT_V({
 		var context;
@@ -101,30 +64,398 @@ Sdl3Audio::Sdl3Audio(const Game_ConfigAudio& cfg) :
 	const int frequency = 44100;
 #endif
 
-	SDL_AudioSpec spec = { SDL_AUDIO_S16, 2, frequency };
-	audio_stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_OUTPUT, &spec, sdl_audio_callback, this);
-
-	if (!audio_stream) {
-		Output::Warning("Couldn't open audio: {}", SDL_GetError());
+	if (SDL_GetAudioDeviceFormat(SDL_AUDIO_DEVICE_DEFAULT_OUTPUT, &device_spec, nullptr) < 0) {
+		Output::Warning("SDL_GetAudioDeviceFormat failed: {}", SDL_GetError());
 		return;
 	}
 
-	SetFormat(spec.freq, sdl_format_to_format(spec.format), spec.channels);
-
-	// Start Audio
-	SDL_ResumeAudioDevice(SDL_GetAudioStreamDevice(audio_stream));
+	int i = 0;
+	for (auto& BGM_Channel : BGM_Channels) {
+		BGM_Channel.id = i++;
+		BGM_Channel.instance = this;
+	}
+	i = 0;
+	for (auto& SE_Channel : SE_Channels) {
+		SE_Channel.id = i++;
+		SE_Channel.instance = this;
+	}
+	BGM_PlayedOnceIndicator = false;
+	midi_thread.reset();
 }
 
-Sdl3Audio::~Sdl3Audio() {
-	// FIXME MIGRATION Crash SDL_CloseAudioDevice(SDL_GetAudioStreamDevice(audio_stream));
+void Sdl3Audio::BGM_Play(Filesystem_Stream::InputStream stream, int volume, int pitch, int fadein) {
+	if (!stream) {
+		Output::Warning("Couldn't play BGM {}: File not readable", stream.GetName());
+		return;
+	}
+
+	for (auto& BGM_Channel : BGM_Channels) {
+		if (!BGM_Channel.IsUsed()) {
+			// If there is an unused bgm channel
+			LockMutex();
+			BGM_PlayedOnceIndicator = false;
+			UnlockMutex();
+			PlayOnChannel(BGM_Channel, std::move(stream), volume, pitch, fadein);
+			return;
+		}
+	}
 }
 
-void Sdl3Audio::LockMutex() const {
-	SDL_LockAudioStream(audio_stream);
+void Sdl3Audio::BGM_Pause() {
+	for (auto& BGM_Channel : BGM_Channels) {
+		if (BGM_Channel.IsUsed()) {
+			BGM_Channel.SetPaused(true);
+		}
+	}
 }
 
-void Sdl3Audio::UnlockMutex() const {
-	SDL_UnlockAudioStream(audio_stream);
+void Sdl3Audio::BGM_Resume() {
+	for (auto& BGM_Channel : BGM_Channels) {
+		if (BGM_Channel.IsUsed()) {
+			BGM_Channel.SetPaused(false);
+		}
+	}
 }
 
-#endif
+void Sdl3Audio::BGM_Stop() {
+	LockMutex();
+	for (auto& BGM_Channel : BGM_Channels) {
+		BGM_Channel.Stop();
+	}
+	BGM_PlayedOnceIndicator = false;
+	UnlockMutex();
+}
+
+bool Sdl3Audio::BGM_PlayedOnce() const {
+	if (BGM_PlayedOnceIndicator) {
+		return BGM_PlayedOnceIndicator;
+	}
+
+	LockMutex();
+	// Audio Decoders set this in the Decoding thread
+	for (auto& BGM_Channel : BGM_Channels) {
+		if (BGM_Channel.midi_out_used) {
+			BGM_PlayedOnceIndicator = midi_thread->GetMidiOut().GetLoopCount() > 0;
+		}
+	}
+	UnlockMutex();
+
+	return BGM_PlayedOnceIndicator;
+}
+
+bool Sdl3Audio::BGM_IsPlaying() const {
+	for (auto& BGM_Channel : BGM_Channels) {
+		if (BGM_Channel.IsUsed()) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int Sdl3Audio::BGM_GetTicks() const {
+	unsigned ticks = 0;
+	LockMutex();
+	for (auto& BGM_Channel : BGM_Channels) {
+		int cur_ticks = BGM_Channel.GetTicks();
+		if (cur_ticks >= 0) {
+			ticks = static_cast<unsigned>(cur_ticks);
+		}
+	}
+	UnlockMutex();
+	return ticks;
+}
+
+void Sdl3Audio::BGM_Fade(int fade) {
+	LockMutex();
+	for (auto& BGM_Channel : BGM_Channels) {
+		BGM_Channel.SetFade(fade);
+	}
+	UnlockMutex();
+}
+
+void Sdl3Audio::BGM_Volume(int volume) {
+	LockMutex();
+	for (auto& BGM_Channel : BGM_Channels) {
+		BGM_Channel.SetVolume(volume);
+	}
+	UnlockMutex();
+}
+
+void Sdl3Audio::BGM_Pitch(int pitch) {
+	LockMutex();
+	for (auto& BGM_Channel : BGM_Channels) {
+		BGM_Channel.SetPitch(pitch);
+	}
+	UnlockMutex();
+}
+
+std::string Sdl3Audio::BGM_GetType() const {
+	std::string type;
+
+	LockMutex();
+	for (auto& BGM_Channel : BGM_Channels) {
+		if (BGM_Channel.IsUsed()) {
+			if (BGM_Channel.midi_out_used) {
+				type = "midi";
+				break;
+			} else {
+				type = BGM_Channel.decoder->GetType();
+				break;
+			}
+		}
+	}
+	UnlockMutex();
+
+	return type;
+}
+
+void Sdl3Audio::SE_Play(std::unique_ptr<AudioSeCache> se, int volume, int pitch) {
+	if (!se) {
+		Output::Warning("SE_Play: AudioSeCache data is NULL");
+		return;
+	}
+
+	for (auto& SE_Channel : SE_Channels) {
+		if (!SE_Channel.sdl_stream || SDL_GetAudioStreamAvailable(SE_Channel.sdl_stream.get()) == 0) {
+			//If there is an unused se channel
+			PlayOnChannel(SE_Channel, std::move(se), volume, pitch);
+			return;
+		}
+	}
+	// FIXME Not displaying as warning because multiple games exhaust free channels available, see #1356
+	Output::Debug("Couldn't play {} SE. No free channel available", se->GetName());
+}
+
+void Sdl3Audio::SE_Stop() {
+	for (auto& SE_Channel : SE_Channels) {
+		if (SE_Channel.sdl_stream) {
+			SE_Channel.sdl_stream.reset();
+		}
+	}
+}
+
+void Sdl3Audio::Update() {
+	for (auto& BGM_Channel : BGM_Channels) {
+		if (BGM_Channel.decoder) {
+			// FIXME
+			BGM_Channel.decoder->Update(std::chrono::milliseconds(16));
+		}
+	}
+}
+
+void sdl_bgm_audio_callback(void* userdata, SDL_AudioStream* stream, int additional_amount, [[maybe_unused]] int total_amount) {
+	if (additional_amount <= 0) {
+		return;
+	}
+
+	static std::vector<uint8_t> buffer;
+	if (buffer.size() < static_cast<size_t>(additional_amount)) {
+		buffer.resize(additional_amount);
+	}
+
+	auto chan = static_cast<Sdl3Audio::BgmChannel*>(userdata);
+	chan->decoder->Decode(buffer.data(), buffer.size());
+
+	int volume = chan->decoder->GetVolume();
+	if (volume != 100) {
+		SDL_MixAudioFormat(buffer.data(), buffer.data(), chan->sdl_spec.format, buffer.size(), volume / 100.0f * SDL_MIX_MAXVOLUME);
+	}
+
+	SDL_PutAudioStreamData(stream, buffer.data(), buffer.size());
+}
+
+bool Sdl3Audio::PlayOnChannel(BgmChannel& chan, Filesystem_Stream::InputStream filestream, int volume, int pitch, int fadein) {
+	if (!filestream) {
+		Output::Warning("BGM file not readable: {}", filestream.GetName());
+		return false;
+	}
+
+	// Midiout is only supported on channel 0 because this is an exclusive resource
+	if (chan.id == 0 && GenericAudioMidiOut::IsSupported(filestream)) {
+		chan.decoder.reset();
+
+		// FIXME: Try Fluidsynth and WildMidi first
+		// If they work fallback to the normal AudioDecoder handler below
+		// There should be a way to configure the order
+		if (!MidiDecoder::CreateFluidsynth(true) && !MidiDecoder::CreateWildMidi(true)) {
+			if (!midi_thread) {
+				midi_thread = std::make_unique<GenericAudioMidiOut>();
+				if (midi_thread->IsInitialized()) {
+					midi_thread->StartThread();
+				} else {
+					midi_thread.reset();
+				}
+			}
+
+			if (midi_thread) {
+				midi_thread->LockMutex();
+				auto &midi_out = midi_thread->GetMidiOut();
+				if (midi_out.Open(std::move(filestream))) {
+					midi_out.SetPitch(pitch);
+					midi_out.SetVolume(0);
+					midi_out.SetFade(volume, std::chrono::milliseconds(fadein));
+					midi_out.SetLooping(true);
+					midi_out.Resume();
+					chan.midi_out_used = true;
+					midi_thread->UnlockMutex();
+					return true;
+				}
+				midi_thread->UnlockMutex();
+			}
+		}
+	}
+
+	if (midi_thread) {
+		midi_thread->GetMidiOut().Reset();
+	}
+
+	chan.decoder = AudioDecoder::Create(filestream);
+	chan.midi_out_used = false;
+	if (chan.decoder && chan.decoder->Open(std::move(filestream))) {
+		Format format;
+		chan.decoder->GetFormat(format.frequency, format.format, format.channels);
+		chan.decoder->SetPitch(pitch);
+		chan.decoder->SetVolume(0);
+		chan.decoder->SetFade(volume, std::chrono::milliseconds(fadein));
+		chan.decoder->SetLooping(true);
+
+		auto sdl_format = to_sdl_format(format.format);
+		if (sdl_format == 0) {
+			sdl_format = SDL_AUDIO_S16;
+			chan.decoder->SetFormat(format.frequency, AudioDecoder::Format::S16, format.channels);
+		}
+
+		chan.sdl_spec = { sdl_format, format.channels, format.frequency };
+		chan.sdl_stream.reset(SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_OUTPUT, &chan.sdl_spec, sdl_bgm_audio_callback, &chan));
+
+		if (!chan.sdl_stream) {
+			Output::Warning("SDL_CreateAudioStream failed: {}", SDL_GetError());
+			return false;
+		}
+
+		if (SDL_ResumeAudioDevice(SDL_GetAudioStreamDevice(chan.sdl_stream.get())) < 0) {
+			Output::Warning("SDL_ResumeAudioDevice failed: {}", SDL_GetError());
+			chan.sdl_stream.reset();
+			return false;
+		}
+
+		return true;
+	} else {
+		Output::Warning("Couldn't play BGM {}. Format not supported", filestream.GetName());
+	}
+
+	return false;
+}
+
+bool Sdl3Audio::PlayOnChannel(SeChannel& chan, std::unique_ptr<AudioSeCache> se, int volume, int pitch) {
+	Format format;
+	chan.decoder = se->CreateSeDecoder();
+	chan.decoder->SetPitch(pitch);
+	chan.decoder->GetFormat(format.frequency, format.format, format.channels);
+	chan.decoder->SetVolume(volume);
+
+	bool use_raw_buffer = true;
+	auto sdl_format = to_sdl_format(format.format);
+	if (sdl_format == 0) {
+		sdl_format = SDL_AUDIO_S16;
+		chan.decoder->SetFormat(format.frequency, AudioDecoder::Format::S16, format.channels);
+		use_raw_buffer = false;
+	}
+
+	chan.sdl_spec = { sdl_format, format.channels, format.frequency };
+	chan.sdl_stream.reset(SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_OUTPUT, &chan.sdl_spec, nullptr, nullptr));
+
+	if (!chan.sdl_stream) {
+		Output::Warning("SDL_CreateAudioStream failed: {}", SDL_GetError());
+		return false;
+	}
+
+	if (use_raw_buffer && pitch == 100) {
+		auto se_ref = se->GetSeData();
+		auto& buf = se_ref->buffer;
+
+		int volume = chan.decoder->GetVolume();
+		if (volume != 100) {
+			auto buf_cp = buf;
+			SDL_MixAudioFormat(buf_cp.data(), buf.data(), sdl_format, buf.size(), volume / 100.0f * SDL_MIX_MAXVOLUME);
+			SDL_PutAudioStreamData(chan.sdl_stream.get(), buf_cp.data(), buf_cp.size());
+		} else {
+			SDL_PutAudioStreamData(chan.sdl_stream.get(), buf.data(), buf.size());
+		}
+	} else {
+		auto buf = chan.decoder->DecodeAll();
+
+		int volume = chan.decoder->GetVolume();
+		if (volume != 100) {
+			SDL_MixAudioFormat(buf.data(), buf.data(), sdl_format, buf.size(), volume / 100.0f * SDL_MIX_MAXVOLUME);
+		}
+
+		SDL_PutAudioStreamData(chan.sdl_stream.get(), buf.data(), buf.size());
+	}
+
+	if (SDL_ResumeAudioDevice(SDL_GetAudioStreamDevice(chan.sdl_stream.get())) < 0) {
+		Output::Warning("SDL_ResumeAudioDevice failed: {}", SDL_GetError());
+		chan.sdl_stream.reset();
+		return false;
+	}
+
+	return true;
+}
+
+void Sdl3Audio::BgmChannel::Stop() {
+	if (midi_out_used) {
+		midi_out_used = false;
+		instance->midi_thread->GetMidiOut().Reset();
+		instance->midi_thread->GetMidiOut().Pause();
+	} else if (decoder) {
+		sdl_stream.reset();
+		decoder.reset();
+	}
+}
+
+void Sdl3Audio::BgmChannel::SetPaused(bool newPaused) {
+	if (midi_out_used) {
+		if (newPaused) {
+			instance->midi_thread->GetMidiOut().Pause();
+		} else {
+			instance->midi_thread->GetMidiOut().Resume();
+		}
+	}
+}
+
+int Sdl3Audio::BgmChannel::GetTicks() const {
+	if (midi_out_used) {
+		return instance->midi_thread->GetMidiOut().GetTicks();
+	} else if (decoder) {
+		return decoder->GetTicks();
+	}
+	return -1;
+}
+
+void Sdl3Audio::BgmChannel::SetFade(int fade) {
+	if (midi_out_used) {
+		instance->midi_thread->GetMidiOut().SetFade(0, std::chrono::milliseconds(fade));
+	} else if (decoder) {
+		decoder->SetFade(0, std::chrono::milliseconds(fade));
+	}
+}
+
+void Sdl3Audio::BgmChannel::SetVolume(int volume) {
+	if (midi_out_used) {
+		instance->midi_thread->GetMidiOut().SetVolume(volume);
+	} else if (decoder) {
+		decoder->SetVolume(volume);
+	}
+}
+
+void Sdl3Audio::BgmChannel::SetPitch(int pitch) {
+	if (midi_out_used) {
+		instance->midi_thread->GetMidiOut().SetPitch(pitch);
+	} else if (decoder) {
+		decoder->SetPitch(pitch);
+	}
+}
+
+bool Sdl3Audio::BgmChannel::IsUsed() const {
+	return decoder || midi_out_used;
+}
--- src/platform/sdl/sdl3_audio.h
+++ src/platform/sdl/sdl3_audio.h
@@ -18,15 +18,94 @@
 #ifndef EP_AUDIO_SDL3_H
 #define EP_AUDIO_SDL3_H
 
-#include "audio_generic.h"
+#include "audio.h"
+#include "audio_secache.h"
+#include "audio_decoder_base.h"
+#include "audio_generic_midiout.h"
+#include <SDL.h>
+#include <SDL_audio.h>
+#include <memory>
 
-class Sdl3Audio : public GenericAudio {
+class Sdl3Audio : public AudioInterface {
 public:
 	Sdl3Audio(const Game_ConfigAudio& cfg);
-	~Sdl3Audio();
+	virtual ~Sdl3Audio() = default;
 
-	void LockMutex() const override;
-	void UnlockMutex() const override;
+	void BGM_Play(Filesystem_Stream::InputStream stream, int volume, int pitch, int fadein) override;
+	void BGM_Pause() override;
+	void BGM_Resume() override;
+	void BGM_Stop() override;
+	bool BGM_PlayedOnce() const override;
+	bool BGM_IsPlaying() const override;
+	int BGM_GetTicks() const override;
+	void BGM_Fade(int fade) override;
+	void BGM_Volume(int volume) override;
+	void BGM_Pitch(int pitch) override;
+	std::string BGM_GetType() const override;
+
+	void SE_Play(std::unique_ptr<AudioSeCache> se, int volume, int pitch) override;
+	void SE_Stop() override;
+	virtual void Update() override;
+
+	void vGetConfig(Game_ConfigAudio&) const override {}
+
+	void LockMutex() const {};
+	void UnlockMutex() const {};
+
+	struct SdlAudioDeleter {
+		void operator()(SDL_AudioStream* a) const {
+			if (a) {
+				SDL_DestroyAudioStream(a);
+			}
+		}
+	};
+
+	struct AudioChannel {
+		int id;
+		std::unique_ptr<AudioDecoderBase> decoder;
+		Sdl3Audio* instance = nullptr;
+		std::unique_ptr<SDL_AudioStream, SdlAudioDeleter> sdl_stream;
+		SDL_AudioSpec sdl_spec;
+	};
+
+	struct BgmChannel : public AudioChannel {
+		bool midi_out_used = false;
+		void Stop();
+		void SetPaused(bool newPaused);
+		int GetTicks() const;
+		void SetFade(int fade);
+		void SetVolume(int volume);
+		void SetPitch(int pitch);
+		bool IsUsed() const;
+	};
+
+	struct SeChannel : public AudioChannel {
+	};
+private:
+	struct Format {
+		int frequency;
+		AudioDecoder::Format format;
+		int channels;
+	};
+	SDL_AudioSpec device_spec;
+
+	bool PlayOnChannel(BgmChannel& chan, Filesystem_Stream::InputStream stream, int volume, int pitch, int fadein);
+	bool PlayOnChannel(SeChannel& chan, std::unique_ptr<AudioSeCache> se, int volume, int pitch);
+
+	static constexpr unsigned nr_of_se_channels = 31;
+	static constexpr unsigned nr_of_bgm_channels = 2;
+
+	BgmChannel BGM_Channels[nr_of_bgm_channels];
+	SeChannel SE_Channels[nr_of_se_channels];
+	mutable bool BGM_PlayedOnceIndicator;
+	bool Muted;
+
+	std::vector<int16_t> sample_buffer = {};
+	std::vector<uint8_t> scrap_buffer = {};
+	unsigned scrap_buffer_size = 0;
+	std::vector<float> mixer_buffer = {};
+
+	std::unique_ptr<GenericAudioMidiOut> midi_thread;
 };
 
 #endif
